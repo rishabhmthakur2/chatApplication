76ed7f8c271a9f938f52a4e2a0ce2cfa23a31241
rishabhmthakur2 <rishabhmthakur@outlook.com>
2020-06-25 23:28:53 +0530

    Setting up socket.io

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const publicDirectory = path.join(__dirname, '../public');
+
 app.use(express.static(publicDirectory));
 
-app.listen(3000, ()=>{
+io.on('connection', ()=>{
+    console.log('New Web Socket Connection');
+});
+
+server.listen(3000, () => {
     console.log('Server listening on 3000');
 });
