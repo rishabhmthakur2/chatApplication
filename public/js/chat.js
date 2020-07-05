49b1b3fcefd1ca1d1aacc80aba6090677bb656ec
rishabhmthakur2 <rishabhmthakur@outlook.com>
2020-07-05 21:48:06 +0530

    Enabled Sockets to send and receive messages and receive acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,42 @@
-io()
+const socket = io();
+
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+
+socket.on('message', (message) => {
+    console.log(message);
+});
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    $messageFormButton.setAttribute('disabled', 'disabled');
+
+
+    const message = e.target.elements.message.value;
+
+    socket.emit('sendMessage', message, () => {
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = "";
+        $messageFormInput.focus();
+        console.log('Message delivered!');
+    });
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser');
+    }
+    $sendLocationButton.setAttribute('disabled', 'disabled');
+    navigator.geolocation.getCurrentPosition((position) => {
+        const message = `https://google.com/maps?q=${position.coords.latitude},${position.coords.longitude}`;
+        socket.emit('sendLocation', message, () => {
+            $sendLocationButton.removeAttribute('disabled');
+            console.log('Location Delivered!');
+        });
+
+    });
+});
+
--- src/index.js
+++ src/index.js
@@ -11,9 +11,23 @@ const publicDirectory = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectory));
 
-io.on('connection', ()=>{
+io.on('connection', (socket) => {
     console.log('New Web Socket Connection');
+    socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined!');
+    socket.on('sendMessage', (message, callback) => {
+        io.emit('message', message);
+        callback();
+    });
+    socket.on('sendLocation',(location, callback)=>{
+        io.emit('message', location);
+        callback();
+    });
+    socket.on('disconnect',()=>{
+        io.emit('message', 'A user has left');
+    });
 });
+
 
 server.listen(3000, () => {
     console.log('Server listening on 3000');
